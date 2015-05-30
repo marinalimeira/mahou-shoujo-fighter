b8012bc2a27c1acc974abcc8165fc07c9b7619bb
Marina Limeira <marinaflessa@gmail.com>
2015-05-30 12:15:26 -0300

    Add event to close window

--- main.c
+++ main.c
@@ -6,35 +6,48 @@
 #define WIDTH 600
 
 void init(){
-  if (al_init()){
-    printf("Allegro started.\n");
-  }
+  if (!al_init() ? printf("Failed to initialize Allegro.\n") : 0); 
 
-  if (al_install_keyboard()){
-    printf("Keyboard installed.\n");
-  }
+  if (!al_install_keyboard() ? printf("Failed to install Keyboard.\n") : 0);
+
+  if (!al_init_image_addon() ? printf("Failed to initialize add-on allegro_image.\n") : 0);
 }
 
-int main(int argc, int **argv){
+void main(int argc, int **argv){
   
   ALLEGRO_DISPLAY *display = NULL;
   ALLEGRO_BITMAP *background = NULL;
+  ALLEGRO_EVENT_QUEUE *event_queue = NULL;
 
   init();
 
-  al_init_image_addon();
+  display = al_create_display(HEIGHT, WIDTH);
+  if (!display ? printf("Failed to create display.\n") : 0);
 
   background = al_load_bitmap("imgs/bg.jpg");
-  printf("Background loaded.\n");
+  if (!background ? printf("Fail to load background.\n") : 0);
 
-  display = al_create_display(HEIGHT, WIDTH);
-  printf("Display created.\n");
+  event_queue = al_create_event_queue();
+
+  al_register_event_source(event_queue, al_get_display_event_source(display));
 
   al_draw_bitmap(background, 0, 0, 0);
 
   al_flip_display();
 
-  al_rest(10.0);
+  while (1) {
+      ALLEGRO_EVENT event;
+      ALLEGRO_TIMEOUT timeout;
+      al_init_timeout(&timeout, 0.05);
+
+      int has_events = al_wait_for_event_until(event_queue, &event, &timeout);
+
+      if (has_events && event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+          break;
+      }
+  }
+
+  al_destroy_event_queue(event_queue);
   al_destroy_display(display);
   
 }
