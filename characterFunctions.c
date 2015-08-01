aea9099d2bb301844e57cebc11aef31bc3a4a647
Marina Limeira <marinaflessa@gmail.com>
2015-08-01 20:20:35 -0300

    Fix indentation and movement during bullet colision

--- characterFunctions.c
+++ characterFunctions.c
@@ -2,14 +2,18 @@ void moveCharacterUp(Character *c);
 void moveCharacterDown(Character *c);
 void moveCharacterLeft(Character *c);
 void moveCharacterRight(Character *c);
+
 void makeHomuraAttack1(Character *c);
 void makeMamiAttack1(Character *c);
 void makeAttack2(Character *c);
+
 void collideBullet(Character *character, Character *enemie);
-void killCharacter(Character *c);
-void animateCharacter(Character *c);
-void stopCharacter(Character *c, int key);
 void drawBullet(Character *c);
+
+void animateCharacter(Character *c);
+void killCharacter(Character *c);
+void stopCharacter(Character *c, int key);
+
 
 void moveCharacterUp(Character *c){
   c->dirY = -1;
@@ -83,6 +87,8 @@ void collideBullet(Character *c, Character *e){
         ((c->bullet.y >= e->y) && (c->bullet.y <= e->current_sprite.width + e->y))){
       c->bullet.fired = false;
       e->current_sprite = e->hurt;
+      e->dirX = 0;
+      e->dirY = 0;
       e->life -= c->bullet.damage;
       if (e->life < 0)
         e->life = 0;
@@ -98,8 +104,7 @@ void killCharacter(Character *c){
 }
 
 void animateCharacter(Character *c){
-  if(++c->current_sprite.frameCount >= c->current_sprite.frameDelay) {
-    
+  if(++c->current_sprite.frameCount >= c->current_sprite.frameDelay) {    
     if (c->live){
       if(++c->current_sprite.curFrame >= c->current_sprite.maxFrame){
         c->current_sprite.curFrame = 0;
@@ -121,8 +126,7 @@ void animateCharacter(Character *c){
 
   if(c->x >= HEIGHT - 128){
     c->x = HEIGHT - 128;
-  }
-  else if (c->x <= 0){
+  } else if (c->x <= 0){
     c->x = 0;
   }
 
@@ -150,8 +154,8 @@ void drawBullet(Character *c){
     c->bullet.fired = true;  
     c->bullet.ready = false;
   } else if (c->bullet.fired){
-      al_draw_bitmap_region(c->bullet.image, 0, 0, c->bullet.heigth, c->bullet.width, c->bullet.x, c->bullet.y, c->bullet.animationDirection);
-      if (c->bullet.x >= 900 || c->bullet.x <= -17)
-        c->bullet.fired = false; 
-    }
+    al_draw_bitmap_region(c->bullet.image, 0, 0, c->bullet.heigth, c->bullet.width, c->bullet.x, c->bullet.y, c->bullet.animationDirection);
+    if (c->bullet.x >= 900 || c->bullet.x <= -17)
+      c->bullet.fired = false; 
+  }
 }
