3b9074fad623c1d5020a5619e8d0e19966ba4d5a
Marina Limeira <marinaflessa@gmail.com>
2015-09-23 11:07:37 -0300

    Insert gameMain.c in main.c
    - Fix bug of Homura's animation

--- src/main.c
+++ src/main.c
@@ -26,7 +26,6 @@ ALLEGRO_BITMAP *cursor = NULL;
 #include "characterFunctions.c"
 #include "initializeClouds.c"
 #include "cloudFunctions.c"
-#include "gameMain.c"
 
 void init();
 
@@ -36,9 +35,9 @@ void main() {
   Character kyubey;
 
   initializeKyubey(&kyubey);
-  
+
   int current = 1;
- 
+
   MenuItem start;
   start.selected = al_load_bitmap("imgs/menu-itens/start_selected.bmp");
   al_convert_mask_to_alpha(start.selected, al_map_rgb(0, 255, 38));
@@ -52,7 +51,7 @@ void main() {
   scores.not_selected = al_load_bitmap("imgs/menu-itens/scores.bmp");
   al_convert_mask_to_alpha(scores.not_selected, al_map_rgb(0, 255, 38));
   scores.current = scores.not_selected;
-  
+
   MenuItem settings;
   settings.selected = al_load_bitmap("imgs/menu-itens/settings_selected.bmp");
   al_convert_mask_to_alpha(settings.selected, al_map_rgb(0, 255, 38));
@@ -68,7 +67,7 @@ void main() {
   ex1t.current = ex1t.not_selected;
 
   while (true) {
-    ALLEGRO_EVENT event;    
+    ALLEGRO_EVENT event;
     al_wait_for_event(event_queue, &event);
 
     if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
@@ -90,7 +89,134 @@ void main() {
         }
       } else if (event.keyboard.keycode == ALLEGRO_KEY_ENTER){
         if (current == 1){
-          gameMain();
+          // TODO: when game ends, back to main menu, or a new menu
+
+          background = al_load_bitmap("imgs/bg.bmp");
+
+          Character homura;
+          Character mami;
+
+          initializeHomura(&homura);
+          initializeMami(&mami);
+
+          Cloud cloud1;
+          Cloud cloud2;
+          Cloud cloud3;
+
+          initializeCloud1(&cloud1);
+          initializeCloud2(&cloud2);
+          initializeCloud3(&cloud3);
+
+          bool done = false;
+          int keyPressed = 0;
+          bool isGameOver = false;
+
+          while (!done) {
+            ALLEGRO_EVENT event;
+            al_wait_for_event(event_queue, &event);
+
+            if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+              done = true;
+            }
+
+            keyPressed = event.keyboard.keycode;
+            if (!isGameOver){
+              if (event.type == ALLEGRO_EVENT_KEY_DOWN) {
+                if (keyPressed == ALLEGRO_KEY_ESCAPE){
+                  done = true;
+                }
+                if (!homura.current_sprite.limited){
+                  if (keyPressed == homura.upKey) {
+                    moveCharacterUp(&homura);
+                  } else if (keyPressed == homura.downKey) {
+                    moveCharacterDown(&homura);
+                  } else if (keyPressed == homura.leftKey) {
+                    homura.dirX = -1;
+                    moveCharacterLeft(&homura);
+                  } else if (keyPressed == homura.rightKey) {
+                    homura.dirX = 1;
+                    moveCharacterRight(&homura);
+                  } else if (keyPressed == homura.attack1Key) {
+                    makeHomuraAttack1(&homura);
+                  } else if (keyPressed == homura.attack2Key) {
+                    makeAttack2(&homura);
+                  }
+                }
+                if (!mami.current_sprite.limited){
+                  if (keyPressed == mami.upKey) {
+                    moveCharacterUp(&mami);
+                  } else if (keyPressed == mami.downKey) {
+                    moveCharacterDown(&mami);
+                  } else if (keyPressed == mami.leftKey) {
+                    mami.dirX = 1;
+                    moveCharacterLeft(&mami);
+                  } else if (keyPressed == mami.rightKey) {
+                    mami.dirX = -1;
+                    moveCharacterRight(&mami);
+                  } else if (keyPressed == mami.attack1Key) {
+                    makeMamiAttack1(&mami);
+                  } else if (keyPressed == mami.attack2Key) {
+                    makeAttack2(&mami);
+                  }
+                }
+              } else if(event.type == ALLEGRO_EVENT_KEY_UP){
+                stopCharacter(&homura, keyPressed);
+                stopCharacter(&mami, keyPressed);
+              }
+            }
+
+            if(event.type == ALLEGRO_EVENT_TIMER) {
+              animateCharacter(&homura);
+              animateCharacter(&mami);
+
+              moveCloud(&cloud1);
+              moveCloud(&cloud2);
+              moveCloud(&cloud3);
+
+              collideBullet(&homura, &mami);
+              collideBullet(&mami, &homura);
+            }
+
+            drawBullet(&homura);
+            drawBullet(&mami);
+
+            if (mami.live){
+              if (mami.life <= 0){
+                killCharacter(&mami);
+                isGameOver = true;
+              }
+            }
+
+            if (homura.live){
+              if (homura.life <= 0){
+                killCharacter(&homura);
+                isGameOver = true;
+              }
+            }
+
+            if (isGameOver)
+              al_draw_text(overFont, al_map_rgb(198, 40, 40), 450, 250, ALLEGRO_ALIGN_CENTER, "GAME OVER");
+            al_draw_bitmap_region(homura.current_sprite.image, homura.current_sprite.curFrame * homura.current_sprite.width,
+                0, homura.current_sprite.width, homura.current_sprite.heigth, homura.x, homura.y, homura.animationDirection);
+            al_draw_bitmap_region(mami.current_sprite.image, mami.current_sprite.curFrame * mami.current_sprite.width, 0,
+                mami.current_sprite.width, mami.current_sprite.heigth, mami.x, mami.y, mami.animationDirection);
+            al_draw_bitmap_region(cloud1.image, 0, 0, 290, 160, cloud1.x, cloud1.y, 0);
+            al_draw_bitmap_region(cloud2.image, 0, 0, 238, 140, cloud2.x, cloud2.y, 0);
+            al_draw_bitmap_region(cloud3.image, 0, 0, 569, 247, cloud3.x, cloud3.y, 0);
+            char homu_life[5]  = "";
+            sprintf(homu_life, "%d", homura.life);
+            al_draw_text(lifeFont, al_map_rgb(74, 20, 140), 120, 10, ALLEGRO_ALIGN_RIGHT, homu_life);
+            char mami_life[5]  = "";
+            sprintf(mami_life, "%d", mami.life);
+            al_draw_text(lifeFont, al_map_rgb(249, 168, 37), 800, 10, ALLEGRO_ALIGN_LEFT, mami_life);
+            al_flip_display();
+            al_draw_bitmap(background, 0, 0, 0);
+          }
+
+          al_destroy_sample(main_song);
+          al_destroy_event_queue(event_queue);
+          al_destroy_display(display);
+
         } else if (current == 4){
           break;
         }
@@ -121,27 +247,28 @@ void main() {
     if(event.type == ALLEGRO_EVENT_TIMER) {
       animateCharacter(&kyubey);
     }
-    
-    al_draw_bitmap_region(kyubey.current_sprite.image, kyubey.current_sprite.curFrame * kyubey.current_sprite.width, 0, kyubey.current_sprite.width, kyubey.current_sprite.heigth, kyubey.x, kyubey.y, kyubey.animationDirection);
+
+    al_draw_bitmap_region(kyubey.current_sprite.image, kyubey.current_sprite.curFrame * kyubey.current_sprite.width, 0,
+        kyubey.current_sprite.width, kyubey.current_sprite.heigth, kyubey.x, kyubey.y, kyubey.animationDirection);
     al_draw_bitmap_region(start.current, 0, 0, 228, 50, 336, 220, 0);
     al_draw_bitmap_region(scores.current, 0, 0, 258, 50, 321, 300, 0);
-    al_draw_bitmap_region(settings.current, 0, 0, 355, 50, 282, 380, 0);    
+    al_draw_bitmap_region(settings.current, 0, 0, 355, 50, 282, 380, 0);
     al_draw_bitmap_region(ex1t.current, 0, 0, 172, 50, 364, 460, 0);
     al_flip_display();
     al_draw_bitmap(background, 0, 0, 0);
   }
-  
+
   al_destroy_sample(main_song);
   al_destroy_event_queue(event_queue);
-  al_destroy_display(display);  
-  
+  al_destroy_display(display);
+
 }
 
 void init(){
-  if (!al_init() ? printf("Failed to initialize Allegro.\n") : 0); 
+  if (!al_init() ? printf("Failed to initialize Allegro.\n") : 0);
 
   if (!al_install_keyboard() ? printf("Failed to install Keyboard.\n") : 0);
- 
+
   if (!al_init_image_addon() ? printf("Failed to initialize add-on allegro_image.\n") : 0);
 
   display = al_create_display(HEIGHT, WIDTH);
@@ -176,7 +303,7 @@ void init(){
   al_start_timer(timer);
 
   al_set_window_title(display, "Mahou Shoujo Fighter");
-  
+
   al_register_event_source(event_queue, al_get_timer_event_source(timer));
   al_register_event_source(event_queue, al_get_keyboard_event_source());
   al_register_event_source(event_queue, al_get_display_event_source(display));
